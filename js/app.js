35d93fa66f237b8b015a43115842e4b88ea79d73
Felipe Coutinho Castilho <castfelipe10@gmail.com>
2025-06-25 19:57:27 -0300

    injetando gsap

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+const boxLinks = document.querySelectorAll(".box-link");
+const links = document.querySelectorAll(".link");
+
 window.onload = () => {
-    const boxLinks = document.querySelectorAll(".box-link");
 
     boxLinks.forEach((boxLink) => {
         const arrow = boxLink.querySelector(".icon-arrow");
@@ -12,4 +14,63 @@ window.onload = () => {
             arrow.classList.remove("icon-arrow-active");
         });
     });
+
+    links.forEach((links) => {
+
+        links.addEventListener("mouseenter", () => {
+            arrow.classList.remove("icon-arrow-active");
+        });
+
+        links.addEventListener("mouseleave", () => {
+            arrow.classList.remove("icon-arrow-active");
+        });
+    })
 };
+
+
+// GSAP ANIMATIONS
+
+gsap.fromTo("#fale-conosco", {
+    opacity: 0,
+    y: -20,
+}, {
+    opacity: 1,
+    y: 0,
+    delay: 0.2
+});
+
+gsap.fromTo("#instagram", {
+    opacity: 0,
+    y: -20,
+}, {
+    opacity: 1,
+    y: 0,
+    delay: 0.4,
+});
+
+gsap.fromTo("#facebook", {
+    opacity: 0,
+    y: -20,
+}, {
+    opacity: 1,
+    y: 0,
+    delay: 0.6,
+});
+
+gsap.fromTo("#linkedin", {
+    opacity: 0,
+    y: -20,
+}, {
+    opacity: 1,
+    y: 0,
+    delay: 0.8,
+});
+
+gsap.fromTo("#site", {
+    opacity: 0,
+    y: -20,
+}, {
+    opacity: 1,
+    y: 0,
+    delay: 1,
+});
